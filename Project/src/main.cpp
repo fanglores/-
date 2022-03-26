c9cc500b149f263b754cbbcc761659703bcc7757
Konstantin <72685417+fanglores@users.noreply.github.com>
2022-03-26 12:24:24 +0300

    Generator states

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -2,6 +2,95 @@
 #include "opencv2/core/utils/logger.hpp"
 #include <iostream>
 
+enum GeneratorMode
+{
+    AUTOMATIC,
+    MANUAL
+};
+
+enum GeneratorState
+{
+    ON,
+    STARTING,
+    STOPPING,
+    OFF
+};
+
+class Camera
+{
+
+};
+
+class Generator
+{
+private:
+    GeneratorState state = GeneratorState::OFF;
+    GeneratorMode mode = GeneratorMode::AUTOMATIC;
+
+    int RunManual()
+    {
+        state = GeneratorState::ON;
+
+        //run in manual mode
+
+        return 0;
+    }
+
+    int RunAutomatic()
+    {
+        state = GeneratorState::ON;
+
+        //run in auto mode
+
+        return 0;
+    }
+
+public:
+    int Start(GeneratorMode start_mode)
+    {
+        state = GeneratorState::STARTING;
+        mode = start_mode;
+
+        //some starting operations here
+
+        if (mode == GeneratorMode::AUTOMATIC) RunAutomatic();
+        else RunManual();
+
+        return 0;
+    }
+
+    int Stop()
+    {
+        state = GeneratorState::STOPPING;
+
+        //some stop operations here
+
+        state = GeneratorState::OFF;
+
+        return 0;
+    }
+};
+
+class CloudStorage
+{
+
+};
+
+class MainMachine
+{
+private:
+    Camera camera;
+    cv::Mat previousPhoto, currentPhoto;
+public:
+    MainMachine()
+    {
+    }
+    void updatePhoto()
+    {
+    }
+};
+
+
 int main()
 {   
     cv::utils::logging::setLogLevel(cv::utils::logging::LogLevel::LOG_LEVEL_SILENT);
@@ -19,7 +108,7 @@ int main()
     {
         camera >> frame;
 
-        cv::imshow("Webcam", frame);
+        cv::imshow("Webcam.ru", frame);
 
         if (cv::waitKey(1) == int('q'))
             break;
