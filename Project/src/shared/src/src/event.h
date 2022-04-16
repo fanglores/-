2e5ef131c010bf5c1415c1791f8fd3756fd5d96f
Konstantin <72685417+fanglores@users.noreply.github.com>
2022-04-16 18:05:13 +0300

    Add files via upload
    cmake .h files

---
+++ Project/src/shared/src/src/event.h
@@ -0,0 +1,29 @@
+#pragma once
+
+enum class Event
+{
+	NONE,
+	PRESSED_START,
+	PRESSED_STOP,
+	EXIT
+};
+
+class IEvent
+{
+public:
+	virtual Event getEvent();
+	virtual char readEvent() = 0;
+};
+
+class ConsoleEvent : public IEvent
+{
+public:
+	char readEvent() override;
+};
+
+class TCPEvent : public IEvent
+{
+public:
+	char readEvent() override;
+};
+
